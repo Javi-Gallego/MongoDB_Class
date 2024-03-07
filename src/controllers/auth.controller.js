29ef59caefc69cfb0edf3317d5ae4b9749c7d709
Javi-Gallego <galgar@gmail.com>
2024-03-07 12:38:11 +0100

    feat: added login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
 
 export const register = async (req, res) => {
@@ -44,10 +45,70 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.status(200).json(
-        {
-            success: true,
-            message: "Login user successfully"
+    try {
+        const email = req.body.email
+        const password = req.body.password
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and password are mandatories"
+            })
         }
-    )
+
+        const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        if (!validEmail.test(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Email format is not valid"
+            })
+        }
+                //   |  DE MANERA TEMPORAL SE INCLUYE ANY AL NO PODER DELCARARSE USER EN LA VERIFICACION DE CONTRASEÑA
+        const user = await User.findOne(
+            {
+                email: email
+            }
+        )
+
+        if (!user) {
+            res.status(400).json({
+                success: false,
+                message: "Email or password invalid"
+            })
+        }
+
+        const isValidPassword = bcrypt.compareSync(password, user.password)
+
+        if (!isValidPassword) {
+            return res.status(400).json({
+                success: false,
+                message: "Email or password invalid"
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                roleName: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "2h"
+            }
+        )
+
+        res.status(200).json({
+            success: true,
+            message: "User logged succesfully",
+            token: token //MOSTRAMOS EL TOKEN DE MANERA TEMPORAL PARA PODER PROBAR CON ÉL OTRA FUNCIONALIDADES
+        })
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: "User cant be logged",
+            error: error
+        })
+    }
 }
